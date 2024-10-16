b3b4843f43890ecc262be32c8cf3d8e4dbe99f55
Coriandrum sativum <123120513+PureCilantro@users.noreply.github.com>
2024-10-15 20:03:33 -0600

    Sync

---
+++ screens/ContentScreen.js
@@ -0,0 +1,11 @@
+import SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('db.db');
+
+export default function ContentScreen() {    
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
